3215372cfea2092b8ec5600c88e4f527df371b53
ttasnim5 <tahsintasnim6@gmail.com>
2024-01-22 12:27:06 -0500

    finish Nanami Level 0, greetings and goodbyes + a moderately okay design at the top

---
+++ Nanami_L0.java
@@ -0,0 +1,7 @@
+public class Nanami_L0 {
+    public static void main(String args[]) {
+        System.out.println("» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «» ¤ «");
+        System.out.println("» Hello. I am Nanami, your taskmaster.\n» What can I do for you?\n");
+        System.out.println("» Until next time. Goodbye.");
+    }
+}
