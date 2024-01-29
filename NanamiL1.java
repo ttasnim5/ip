ebb5b367edf5e5a29dc6c722494f133128204eee
ttasnim5 <tahsintasnim6@gmail.com>
2024-01-29 02:17:24 -0500

    edit file to include continued echo + a prompted goodbye

--- NanamiL1.java
+++ NanamiL1.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// import packages to read user input from console
+
 public class NanamiL1 {
     public static void main(String args[]) {
+        // driver code to beign the application
         giveIntroduction();
-        giveFarewell();
+        processUserInputWrapper();
     }
 
     private static void giveIntroduction() {
@@ -19,5 +25,34 @@ public class NanamiL1 {
     private static void giveFarewell() {
         // print a farewell to user
         System.out.println("» Until next time. Goodbye.");
+    }
+
+    private static void echo(String input) {
+        // repeat user input back to user
+        System.out.println("» "+ input);
+    }
+
+    private static void processUserInput() throws IOException {
+        // internal function to read a user's input, process the meaning, and call functions accordingly
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String input = in.readLine();
+
+        if (input.equals("bye") || input.equals("Bye")) {
+            giveFarewell();
+        }
+        else {
+            echo(input);
+            processUserInputWrapper();
+        }
+    }
+
+    private static void processUserInputWrapper() {
+        // wrapper class that calls processUserInput() with  try catch block for exceptions
+        try {
+            processUserInput();
+        } catch (IOException e) {
+            System.out.println("Sorry, I didn't catch that.\n» Try again.");
+            processUserInputWrapper();
+        }
     }
 }
